796c44dc561ce28c6d48616a15d8ea0a0adb90c7
lnap24 <luke.nap@comcast.net>
2024-07-02 11:20:18 -0400

    pushing project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='fibo_sequence',
+      version='0.1',
+      description='The fibo test',
+      url='https://github.com/lnap24/m09_in_class_project',
+      author='LJN',
+      author_email='ljn5yms@virginia.edu',
+      license='MIT',
+      packages=['learningtime']) #must be directory name
